12e19c8977fa2317f96b20c7ef0bc5112c14467d
Sleepy105 <lm.sousa@fe.up.pt>
2021-01-08 12:16:31 +0000

    Use boolean vars when possible and defensive macros

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <stdbool.h>
 #include <util/delay.h>
 
 #define T1TOP 625      // CTC mode OCR1A=625
@@ -11,8 +12,8 @@
 #define MaxTime 500
 
 int16_t time_p; // PWM F=50, therefore we need 500 interrupts to complete 10s
-int8_t state;
-int8_t inv;
+uint8_t state;
+bool inv;
 
 /*********************************************
  * Timer 1 Fast PWM mode
@@ -21,11 +22,11 @@ void timer1_init(void) {
     TCNT1 = 0;     // Set timer1 count zero
     ICR1 = PWMTOP; // TOP count for timer1 -> FPWM = FOSC/(N*(1+TOP)) with
                    // FPWM=50 and N=8
-    TCCR1A = (1 << COM1A1) | (0 << COM1A0);            // Non inverter PWM
-    TCCR1A |= (1 << WGM11) | (0 << WGM10);             // Fast PWM: TOP: ICR1
-    TCCR1B = (1 << WGM13) | (1 << WGM12);              // Fast PWM: TOP: ICR1
-    TCCR1B |= (0 << CS12) | (1 << CS11) | (0 << CS10); // Preesc = 8
-    TIMSK1 |= (1 << TOIE1); // Overflow interrupt enable
+    TCCR1A = _BV(COM1A1) | _BV(0 << COM1A0); // Non inverter PWM
+    TCCR1A |= _BV(WGM11) | _BV(0 << WGM10);  // Fast PWM: TOP: ICR1
+    TCCR1B = _BV(WGM13) | _BV(WGM12);        // Fast PWM: TOP: ICR1
+    TCCR1B |= _BV(0 << CS12) | _BV(CS11) | _BV(0 << CS10); // Preesc = 8
+    TIMSK1 |= _BV(TOIE1); // Overflow interrupt enable
 }
 
 void hw_init_interrupt(void) {
@@ -33,7 +34,7 @@ void hw_init_interrupt(void) {
     DDRD &= ~_BV(IR_Sensor);
     PORTD |= _BV(IR_Sensor);
     /* Interrupt request at falling edge for INT0 */
-    EICRA |= (2 << ISC00);
+    EICRA |= _BV(ISC01);
     /* Enable INT0 */
     EIMSK |= _BV(INT0);
     /* Enable global interrupt flag */
@@ -44,8 +45,8 @@ void hw_init_interrupt(void) {
  * Setup - Output's and Input's
  *********************************************/
 void setup_indv(void) {
-    DDRB |= (1 << Motor_indv); // Output Port (Port 9)
-    DDRB |= _BV(LED);          // LED test
+    DDRB |= _BV(Motor_indv); // Output Port (Port 9)
+    DDRB |= _BV(LED);        // LED test
 
     PORTB &= ~(_BV(LED)); // Clear Output Port
 }
@@ -63,12 +64,12 @@ int main(void) {
     start_timer1(MaxTime); // Reset Timer
     sei();                 // Enable global int
     state = 0;
-    inv = 0;
+    inv = false;
 
     while (1) {
         switch (state) {
         case 0:
-            if (0 == inv) {
+            if (false == inv) {
                 state = 1;             // Normal Rotation
                 start_timer1(MaxTime); // Reset Timer
             } else {
@@ -80,7 +81,7 @@ int main(void) {
         case 1:
             if (0 == time_p) {
                 state = 0;
-                inv = 1; // Set Inverted rotation
+                inv = true; // Set Inverted rotation
                 _delay_ms(1000);
             }
             break;
@@ -88,7 +89,7 @@ int main(void) {
         case 2:
             if (0 == time_p) {
                 state = 0;
-                inv = 0; // Set Normal Rotation
+                inv = false; // Set Normal Rotation
                 _delay_ms(1000);
             }
             break;
@@ -105,20 +106,6 @@ int main(void) {
             OCR1A = 4000; // Pulse with 1ms (-90 degrees / CW max speed)
             break;
         }
-
-        /*
-
-        OCR1A = 2000;                                             // Pulse with
-        1ms (90 degrees / CCW max speed) start_timer1 (MaxTime); _delay_ms
-        (1000);
-
-        OCR1A = 3000;                                             // Pulse
-        with 1.5ms (0 degrees / STOP) _delay_ms (1000);
-
-        OCR1A = 4000;                                             // Pulse with
-        1ms (-90 degrees / CW max speed) _delay_ms (1000);
-
-       */
     }
 }
 
