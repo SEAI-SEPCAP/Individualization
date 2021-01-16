5740ad1980c47115acb5d19f5516167d517e6372
Nuno Lobo <up201604166@fe.up.pt>
2021-01-16 16:39:33 +0000

    Merged Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <stdbool.h>
@@ -7,7 +8,7 @@
 #define FCPU 16000000ul
 
 #define T1TOP 625    // CTC mode OCR1A=625
-#define PWMTOP 39999 // TIMER1 TOP for PRESC=8
+#define PWMTOP 39999 // TIMER TOP for PRESC=8
 
 #define Motor_indv PINL3 // Individualization Servo Output Port - PORT 46
 #define IR_Sensor PIND0  // INT0 - IR Sensor Interrupt
@@ -18,20 +19,24 @@
 #define ZERO_W 1.5  // Duty-cycle for STOP Disk
 #define P_FCLK 2000 // (16M/1000)/8
 
-#define MAX1 1500 // aberto
-#define ZERO1 2800
-#define MAX3 1400 // aberto
-#define ZERO3 2800
-#define MAX2 1600
+#define ZERO1 2800 // fechado
 #define ZERO2 2800
-#define MAX4 1700
+#define ZERO3 2800
 #define ZERO4 2800
-#define MAX5 1900
 #define ZERO5 3100
-#define MAX6 2700
 #define ZERO6 3700
-#define MAX7 1800
 #define ZERO7 3400
+
+#define MAX1 1500 // aberto
+#define MAX2 1600
+#define MAX3 1400
+#define MAX4 1700
+#define MAX5 1900
+#define MAX6 2700
+#define MAX7 1800
+
+#define MAX(index) (MAX##index)
+#define ZERO(index) (ZERO##index)
 
 int16_t time_p; // PWM F=50, therefore we need 500 interrupts to complete 10s
 uint8_t state;
@@ -42,7 +47,7 @@ uint8_t addr;
 uint8_t aux;
 
 void speed(double speed) {
-    if (speed >= 0) {
+    if (speed <= 0) {
         OCR5A = (ZERO_W - abs((speed / 100) * (MAX_CCW - ZERO_W))) * P_FCLK;
     } else {
         OCR5A = (ZERO_W + abs((speed / 100) * (MAX_CW - ZERO_W))) * P_FCLK;
@@ -54,32 +59,31 @@ void speed(double speed) {
  * Timer 1 with interrupt
  *********************************************/
 void timers(void) {
-    /* TCNT1 = 0;     // Set timer1 count zero
-     ICR1 = PWMTOP; // TOP count for timer1 -> FPWM = FOSC/(N*(1+TOP)) with
-                    // FPWM=50 and N=8
-     TCCR1A = _BV(COM1B1) | (0 << COM1A0);            // Non inverter PWM
-     TCCR1A |= _BV(WGM11) | (0 << WGM10);             // Fast PWM: TOP: ICR1
-     TCCR1B = _BV(WGM13) | _BV(WGM12);                // Fast PWM: TOP: ICR1
-     TCCR1B |= (0 << CS12) | _BV(CS11) | (0 << CS10); // Preesc = 8
+    TCNT1 = 0;     // Set timer1 count zero
+    ICR1 = PWMTOP; // TOP count for timer1 -> FPWM = FOSC/(N*(1+TOP)) with
+                   // FPWM=50 and N=8
+    TCCR1A = _BV(COM1A1) | _BV(COM1B1) | (0 << COM1A0); // Non inverter PWM
+    TCCR1A |= _BV(WGM11) | (0 << WGM10);                // Fast PWM: TOP: ICR1
+    TCCR1B = _BV(WGM13) | _BV(WGM12);                   // Fast PWM: TOP: ICR1
+    TCCR1B |= (0 << CS12) | _BV(CS11) | (0 << CS10);    // Preesc = 8
 
-     TCNT3 = 0;     // Set timer1 count zero
-     ICR3 = PWMTOP; // TOP count for timer1 -> FPWM = FOSC/(N*(1+TOP)) with
-                    // FPWM=50 and N=8
-     TCCR3A = _BV(COM3B1) | _BV(COM3C1) | _BV(COM3A1) |
-              (0 << COM3A0);                          // Non inverter PWM
-     TCCR3A |= _BV(WGM31) | (0 << WGM30);             // Fast PWM: TOP: ICR1
-     TCCR3B = _BV(WGM33) | _BV(WGM32);                // Fast PWM: TOP: ICR1
-     TCCR3B |= (0 << CS32) | _BV(CS31) | (0 << CS30); // Preesc = 8
+    TCNT3 = 0;     // Set timer1 count zero
+    ICR3 = PWMTOP; // TOP count for timer1 -> FPWM = FOSC/(N*(1+TOP)) with
+                   // FPWM=50 and N=8
+    TCCR3A = _BV(COM3B1) | _BV(COM3C1) | _BV(COM3A1) |
+             (0 << COM3A0);                          // Non inverter PWM
+    TCCR3A |= _BV(WGM31) | (0 << WGM30);             // Fast PWM: TOP: ICR1
+    TCCR3B = _BV(WGM33) | _BV(WGM32);                // Fast PWM: TOP: ICR1
+    TCCR3B |= (0 << CS32) | _BV(CS31) | (0 << CS30); // Preesc = 8
 
-     TCNT4 = 0;     // Set timer1 count zero
-     ICR4 = PWMTOP; // TOP count for timer1 -> FPWM = FOSC/(N*(1+TOP)) with
-                    // FPWM=50 and N=8
-     TCCR4A = _BV(COM4B1) | _BV(COM4C1) | _BV(COM4A1) |
-              (0 << COM4A0);                          // Non inverter PWM
-     TCCR4A |= _BV(WGM41) | (0 << WGM40);             // Fast PWM: TOP: ICR1
-     TCCR4B = _BV(WGM43) | _BV(WGM42);                // Fast PWM: TOP: ICR1
-     TCCR4B |= (0 << CS42) | _BV(CS41) | (0 << CS40); // Preesc = 8
- */
+    TCNT4 = 0;     // Set timer1 count zero
+    ICR4 = PWMTOP; // TOP count for timer1 -> FPWM = FOSC/(N*(1+TOP)) with
+                   // FPWM=50 and N=8
+    TCCR4A = _BV(COM4B1) | _BV(COM4C1) | _BV(COM4A1) |
+             (0 << COM4A0);                          // Non inverter PWM
+    TCCR4A |= _BV(WGM41) | (0 << WGM40);             // Fast PWM: TOP: ICR1
+    TCCR4B = _BV(WGM43) | _BV(WGM42);                // Fast PWM: TOP: ICR1
+    TCCR4B |= (0 << CS42) | _BV(CS41) | (0 << CS40); // Preesc = 8
 
     TCNT5 = 0;     // Set timer5 count zero
     ICR5 = PWMTOP; // TOP count for timer5 -> FPWM = FOSC/(N*(1+TOP)) with
@@ -89,15 +93,6 @@ void timers(void) {
     TCCR5B = _BV(WGM53) | _BV(WGM52);                // Fast PWM: TOP: ICR5
     TCCR5B |= (0 << CS52) | _BV(CS51) | (0 << CS50); // Preesc = 8
     TIMSK5 |= _BV(TOIE5); // Overflow interrupt enable
-
-    /* OCR3B = ZERO1;
-     OCR3C = ZERO2;
-     OCR1B = ZERO3;
-     OCR3A = ZERO4;
-     OCR4A = ZERO5;
-     OCR4B = ZERO6;
-     OCR4C = ZERO7;
-     */
 }
 
 /*********************************************
@@ -114,8 +109,6 @@ void IR_interrupt(void) {
     EICRA |= _BV(ISC01);
     /* Enable INT0 */
     EIMSK |= _BV(INT0);
-    /* Enable global interrupt flag */
-    sei();
 }
 
 /*********************************************
@@ -130,13 +123,13 @@ void setup_indv(void) {
  *********************************************/
 void setup_dist(void) {
     // SERVO as OUT
-    DDRE |= 1 << PINE4; // Digital Pin 2, OC3B
-    DDRE |= 1 << PINE5; // Digital Pin 3, OC3C
-    DDRB |= 1 << PINB6; // Digital Pin 12, OC1B
-    DDRE |= 1 << PINE3; // Digital Pin 5, OC3A
-    DDRH |= 1 << PINH3; // Digital Pin 6, OC4A
-    DDRH |= 1 << PINH4; // Digital Pin 7, OC4B
-    DDRH |= 1 << PINH5; // Digital Pin 8, OC4C
+    DDRE |= _BV(PINE4); // Digital Pin 2, OC3B
+    DDRE |= _BV(PINE5); // Digital Pin 3, OC3C
+    DDRB |= _BV(PINB6); // Digital Pin 12, OC1B
+    DDRE |= _BV(PINE3); // Digital Pin 5, OC3A
+    DDRH |= _BV(PINH3); // Digital Pin 6, OC4A
+    DDRH |= _BV(PINH4); // Digital Pin 7, OC4B
+    DDRH |= _BV(PINH5); // Digital Pin 8, OC4C
 }
 
 /*********************************************
@@ -175,11 +168,11 @@ void indv_control(void) {
         // OCR1A = 3000; // Pulse with 1.5ms (0 degrees / STOP)
         break;
     case 1:
-        speed(-70);
+        speed(70);
         // OCR1A = 2800; // Pulse with 1ms (90 degrees / CCW max speed)
         break;
     case 2:
-        speed(70);
+        speed(-70);
         // OCR1A = 4000; // Pulse with 1ms (-90 degrees / CW max speed)
         break;
     }
@@ -189,58 +182,58 @@ void dist_control(void) {
 
     switch (selected_servo) {
     case 1:
-        // delay(800);
-        OCR3B = MAX1;
-        // delay(800);
-        OCR3B = ZERO1;
+        _delay_ms(800);
+        OCR3B = MAX(1);
+        _delay_ms(800);
+        OCR3B = ZERO(1);
         break;
 
     case 2:
-        // delay(800);
-        OCR3C = MAX2;
-        // delay(800);
+        _delay_ms(800);
+        OCR3C = MAX(2);
+        _delay_ms(800);
         // now scan back from 180 to 0 degrees
-        OCR3C = ZERO2;
+        OCR3C = ZERO(2);
         break;
 
     case 3:
-        // delay(800);
-        OCR1A = MAX3;
-        // delay(800);
+        _delay_ms(800);
+        OCR1A = MAX(3);
+        _delay_ms(800);
         // now scan back from 180 to 0 degrees
-        OCR1A = ZERO3;
+        OCR1A = ZERO(3);
         break;
 
     case 4:
-        // delay(800);
-        OCR3A = MAX4;
-        // delay(800);
+        _delay_ms(800);
+        OCR3A = MAX(4);
+        _delay_ms(800);
         // now scan back from 180 to 0 degrees
-        OCR3A = ZERO4;
+        OCR3A = ZERO(4);
         break;
 
     case 5:
-        // delay(800);
-        OCR4A = MAX5;
-        // delay(800);
+        _delay_ms(800);
+        OCR4A = MAX(5);
+        _delay_ms(800);
         // now scan back from 180 to 0 degrees
-        OCR4A = ZERO5;
+        OCR4A = ZERO(5);
         break;
 
     case 6:
-        // delay(800);
-        OCR4B = MAX6;
-        // delay(800);
+        _delay_ms(800);
+        OCR4B = MAX(6);
+        _delay_ms(800);
         // now scan back from 180 to 0 degrees
-        OCR4B = ZERO6;
+        OCR4B = ZERO(6);
         break;
 
     case 7:
-        // delay(800);
-        OCR4C = MAX7;
-        // delay(800);
+        _delay_ms(800);
+        OCR4C = MAX(7);
+        _delay_ms(800);
         // now scan back from 180 to 0 degrees
-        OCR4C = ZERO7;
+        OCR4C = ZERO(7);
         break;
 
     default:
@@ -280,7 +273,7 @@ void send_Data(uint8_t Data) {
 }
 
 void initialization(void) {
-    // setup_dist();
+    setup_dist();
     setup_indv();
 
     timers();
@@ -288,10 +281,9 @@ void initialization(void) {
     IR_interrupt();
     // setup_uart();
 
-    start_timer5(MaxTime); // Reset Timer
-    sei();                 // Enable global int
+    sei(); // Enable global int
 
-    OCR5A = 2800;
+    start_timer5(MaxTime); // Reset Timer
 
     state = 0;
     inv = false;
@@ -304,13 +296,11 @@ void initialization(void) {
 int main(void) {
     // Initialization
     initialization();
-    // selected_servo = 1;
     while (1) {
         // receive_Data();
         indv_control();
         // if (addr == 0x03) {
-        // dist_control();
-        // selected_servo++;
+        dist_control();
         //}
     }
 }
